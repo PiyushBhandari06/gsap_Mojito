5309bdff5cfe52ba1579996212250d3986fee03d
PiyushBhandari06 <bhandaripiyush2003@gmail.com>
2025-06-30 18:50:55 +0530

    fixed About Section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,25 +4,25 @@ import gsap from 'gsap'
 
 const About = () => {
     useGSAP(() => {
-	const titleSplit = SplitText.create('#about h2', {
-	 type: 'words'
-	})
-	
-	const scrollTimeline = gsap.timeline({
-	 scrollTrigger: {
-		trigger: '#about',
-		start: 'top center'
-	 }
-	})
-	
-	scrollTimeline
-	 .from(titleSplit.words, {
-		opacity: 0, duration: 1, yPercent: 100, ease: 'expo.out', stagger: 0.02
-	})
-	 .from('.top-grid div, .bottom-grid div', {
-		opacity: 0, duration: 1, ease: 'power1.inOut', stagger: 0.04,
-	}, '-=0.5')//this will make this animation start 0.5s before previous (above one) completes
- })
+        const titleSplit = SplitText.create('#about h2', {
+            type: 'words'
+        })
+
+        const scrollTimeline = gsap.timeline({
+            scrollTrigger: {
+                trigger: '#about',
+                start: 'top center'
+            }
+        })
+
+        scrollTimeline
+            .from(titleSplit.words, {
+                opacity: 0, duration: 1, yPercent: 100, ease: 'expo.out', stagger: 0.02
+            })
+            .from('.top-grid div, .bottom-grid div', {
+                opacity: 0, duration: 1, ease: 'power1.inOut', stagger: 0.04,
+            }, '-=0.5')//this will make this animation start 0.5s before previous (above one) completes
+    })
 
     return (
         <div id='about'>
@@ -30,23 +30,21 @@ const About = () => {
                 <div className='content'>
                     <div className='md:col-span-8'>
                         <p className='badge'>Best Cocktails</p>
-                        <h2>Where Every Detail Matters <span className='text-white'>-</span>
-                            from muddle to garnish
+                        <h2>Where Every Detail Matters <span className='text-white'>-</span> from muddle to garnish
                         </h2>
+                    </div>
 
-                        <div className='sub-content'>
-                            <p>Every cocktail we serve is a reflection of our obsession with detail — from the first muddle to the final garnish. That care is what turns a simple drink into something truly memorable. </p>
-                            <div>
-                                <p className='md:text-3xl text-xl font-bold'>
-                                    <span>4.5</span>
-                                    /5
-                                </p>
-                                <p className='text-sm text-white-100'>More than +12000 customers</p>
-                            </div>
+                    <div className='sub-content'>
+                        <p>Every cocktail we serve is a reflection of our obsession with detail — from the first muddle to the final garnish. That care is what turns a simple drink into something truly memorable. </p>
+                        <div>
+                            <p className='md:text-3xl text-xl font-bold'>
+                                <span>4.5</span>/5
+                            </p>
+                            <p className='text-sm text-white-100'>More than +12000 customers</p>
                         </div>
                     </div>
                 </div>
-
+            </div>
 
                 <div className='top-grid'>
                     <div className='md:col-span-3'>
@@ -80,7 +78,6 @@ const About = () => {
                     </div>
 
                 </div>
-            </div>
         </div>
     )
 }
